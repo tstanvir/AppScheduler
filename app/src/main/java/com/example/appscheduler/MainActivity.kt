cd8bf0d67b3492e868c3acd96f4d2cfd1f30fb44
Md Tanvir Hussain <tanvirshossain0@gmail.com>
2025-04-12 20:47:41 +0600

    [issues/10] Fix "app not launching when schedular app is destroyed" [Problem] App not launching when schedular app is destroyed. [Cause] App Overlay permission not requested. [Measure] Now it is requested.
    [Issue path] Schedule any app and clear the background app from the recent task manager.
    
    Signed-off-by: Md Tanvir Hussain <tanvirshossain0@gmail.com>

--- app/src/main/java/com/example/appscheduler/MainActivity.kt
+++ app/src/main/java/com/example/appscheduler/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.appscheduler
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -22,6 +24,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        checkOverlayPermission()
+
         setContent {
             AppSchedulerTheme {
                 Surface(
@@ -35,6 +39,13 @@ class MainActivity : ComponentActivity() {
                     HomeScreen(appListViewModel, scheduleViewModel)
                 }
             }
+        }
+    }
+
+    private fun checkOverlayPermission() {
+        if (!Settings.canDrawOverlays(this)) {
+            val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+            startActivity(myIntent)
         }
     }
 }
